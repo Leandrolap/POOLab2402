0a4e2d1a437d688bcab497b1562fcae9ff34ec7a
Leandro <suporte_desenvolvimento@outlook.com>
2023-03-03 21:21:07 -0300

    Implementa construtores e equals

--- src/br/newtonpaiva/dominio/conta.java
+++ src/br/newtonpaiva/dominio/conta.java
@@ -1,8 +1,25 @@
 package br.newtonpaiva.dominio;
 
+import java.util.Objects;
+
 public class conta {
     private Integer numero;
     private Double saldo;
+
+    public conta(Integer numero)
+    {
+        this(numero, 0.0);
+    }
+
+    public conta(){
+        this(null);
+    }
+
+    public conta(Integer numero, Double saldo)
+    {
+        this.numero = numero;
+        this.saldo = saldo;
+    }
 
     public Double sacar(Double valor){
         return 0.0;
@@ -33,5 +50,18 @@ public class conta {
     public void setSaldo(Double saldo){
 
         this.saldo = saldo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        conta conta = (conta) o;
+        return Objects.equals(numero, conta.numero);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numero);
     }
 }
--- src/br/newtonpaiva/ui/Main.java
+++ src/br/newtonpaiva/ui/Main.java
@@ -4,10 +4,20 @@ import br.newtonpaiva.dominio.conta;
 
 public class Main {
     public static void main(String[] args) {
-        var c = new conta();
+        var c = new conta(10);
         c.setNumero(10);
         c.setSaldo(100.0);
         c.depositar(50.0);
+
+        conta c2 = new conta(10);
+        c2.setNumero(90);
+        c2.setSaldo(500.0);
+
+        if (c.equals(c2)) {
+            System.out.println("Mesma Conta");
+        } else {
+            System.out.println("Contas a pagar");
+        }
 
         System.out.println(c.getSaldo());
     }
